e060c9101ff26a0609e62a09269ad17fc8ed61bb
Akshay Kumar <sunny.akshay1706@gmail.com>
2014-07-29 02:30:04 +0530

    use file api

--- js/tracking.js
+++ js/tracking.js
@@ -19,10 +19,41 @@ if (un=="quest") {
 }
 
 function readFile() {
-    var ForAppending = 8;
-var TristateTrue = -1;
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-var newLine = fso.OpenTextFile("Error.txt", ForAppending, true, TristateTrue);
-newLine.Write("Hello World!\n")
-newLine.Close
+// Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("track.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+        writer.onwriteend = function(evt) {
+            console.log("contents of file now 'some sample text'");
+            writer.truncate(11);  
+            writer.onwriteend = function(evt) {
+                console.log("contents of file now 'some sample'");
+                writer.seek(4);
+                writer.write(" different text");
+                writer.onwriteend = function(evt){
+                    console.log("contents of file now 'some different text'");
+                }
+            };
+        };
+        writer.write("some sample text");
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
 }
