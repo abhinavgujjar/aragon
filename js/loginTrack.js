3af8778b343073c762c9c494e016cf50e063a76e
Akshay Kumar <sunny.akshay1706@gmail.com>
2014-08-07 14:29:13 +0530

    Almost

--- js/loginTrack.js
+++ js/loginTrack.js
@@ -62,7 +62,7 @@ function loginTrack(loginStatus,sessionId) {
     minutes = "0" + minutes
      }     
     writer.seek(writer.length);
-    writer.write(sessionId+","+loginStatus+","+username+","+day + "/" +month+ "/" + year +","+hours + ":" + minutes+"\n");
+    writer.write(sessionId+"_"+username+","+loginStatus+","+username+","+day + "/" +month+ "/" + year +","+hours + ":" + minutes+"\n");
     writer.abort();
     };
 
