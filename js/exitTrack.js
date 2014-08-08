1d3ac46f5fdf42462885e3ec0dfd7a5bbff77cf0
Akshay Kumar <sunny.akshay1706@gmail.com>
2014-08-08 11:35:21 +0530

    Almost

---
+++ js/exitTrack.js
@@ -0,0 +1,42 @@
+function exitTrack(loginStatus,groupId,moduleId,status,sessionId) {
+// Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("Track.csv", {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+    var username = document.loginForm.exampleInputEmail1.value;
+    var currentTime = new Date()
+    var month = currentTime.getMonth() + 1
+    var day = currentTime.getDate()
+    var year = currentTime.getFullYear()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+    minutes = "0" + minutes
+     }
+    writer.seek(writer.length);
+    writer.write(sessionId+"_"+username+","+status+","+username+","+day+ "/" +month+ "/" + year +","+hours + ":" + minutes+","+groupId+","+moduleId+"\n"+sessionId+"_"+username+","+loginStatus+","+username+","+day + "/" +month+ "/" + year +","+hours + ":" + minutes+"\n");
+    writer.abort();
+    writer.onwriteend = function(evt) {
+        console.log("Write Success")
+    };
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+}
